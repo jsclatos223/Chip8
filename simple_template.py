e0862dc18b15f5666806c4fa6696455c96b2c20b
Jeremy Clatos <jeremyclatos@Jeremys-MacBook-Pro.local>
2020-01-16 21:50:14 -0500

    Adding scripts received from Cain.

---
+++ simple_template.py
@@ -0,0 +1,35 @@
+# Import Library Section
+# "import [library_name]"
+# import xxx
+
+# Import from Library Section
+# from [library_name] import [function]
+# from xxx import yyy
+
+
+# Function Definitions
+'''
+It is considered proper python coding syntax to keep the
+beginning and ending parts of a function, seperated by two spaces.
+'''
+def test(argument1, argument2):
+	'''
+	Function Skeleton.
+	A function can take 0 or many arguments.
+	
+	Put a description of the function here.
+	'''
+
+	# The 'pass' command allows python to run this function by 'passing' over it.
+	pass
+
+
+def main():
+	x = ['hello','bye']
+	print(x[1])
+
+
+# Program Execution Start
+if __name__ == "__main__":
+	main()
+
